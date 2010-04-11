219fea85618bff2a118d808a5b1bc66950ca5f06
RainerBlessing <rainer.blessing@googlemail.com>
2010-04-11 17:01:11 +0200

    setting email as login field is unnecessary

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,4 @@
 class User < ActiveRecord::Base
-  acts_as_authentic do |c|
-    c.login_field :email
-  end
-
   def before_connect(facebook_session)
     self.name = facebook_session.user.name
   end
